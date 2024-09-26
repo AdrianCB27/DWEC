cb2a1a89f6d971dcfe2becee43831a06ebce7825
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2024-09-26 08:52:33 +0200

    Ejercicios 6 y 7 terminados

---
+++ Ejercicios JS 2.2/Ejercicio 7/AdrianContrerasBueno2.2.7.js
@@ -0,0 +1,26 @@
+/*
+Comparando
+Utiliza la consola para escribir una expresión que compare dos strings y produzca un valor TRUE.
+Escribí una expresión comparando dos strings que devuelva FALSE.
+Escribí una expresión que involucre un string y un número y devuelva FALSE.
+Escribí una expresión que involucre un string y un número y devuelva TRUE
+*/
+//1
+let a="Hola";
+let b = "Hola";
+console.log(a==b);
+
+//2
+let x="Adiós";
+let y="Adios";
+console.log(x==y);
+
+//3
+let string1="2";
+let numero=2;
+console.log(string1===numero);
+
+//4
+let string2="5";
+let numero2=5;
+console.log(string2==numero2);
